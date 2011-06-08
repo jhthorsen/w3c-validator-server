76a219d63f2bdb570bfce91a80a9484f8670e136
Jan Henning Thorsen <jhthorsen@cpan.org>
2011-06-08 21:22:07 +0200

    0.11     Wed Jun  8 21:22:06 CEST 2011        - Remove P::A::File::DirectoryIndex

--- lib/W3C/Validator/Server.pm
+++ lib/W3C/Validator/Server.pm
@@ -6,7 +6,7 @@ W3C::Validator::Server - Run http://validator.w3c.org as a local instance
 
 =head1 VERSION
 
-0.1002
+0.11
 
 =head1 DESCRIPTION
 
@@ -64,7 +64,7 @@ use strict;
 use warnings;
 use base 'Plack::Runner';
 
-our $VERSION = eval '0.1002';
+our $VERSION = eval '0.11';
 
 =head1 METHODS
 
